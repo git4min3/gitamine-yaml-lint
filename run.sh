6c91c060061adaedc906ee52ca49f99c2bf13463
Javier Gonzalez <xgc1986@gmail.com>
2018-01-07 12:57:17 +0100

    run.sh

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+EVENT=""
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# The reading options part
+while [[ $# -gt 0 ]] && [[ ."$1" = .--* ]] ;
+do
+    opt="$1";
+    shift;              #expose next argument
+    case "$opt" in
+        "--event="* )
+           EVENT="${opt#*=}";;
+   esac
+done
+
+if [ -e "${DIR}/hooks/${EVENT}.sh" ]
+then
+    eval "${DIR}/hooks/${EVENT}.sh $*"
+else
+    echo "This plugin only works on [pre-commit, pre-push] hooks"
+    exit 1
+fi
